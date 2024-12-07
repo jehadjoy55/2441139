682a35562ad848e63a9f0b3472a6fcb068c2723f
jehadjoy55 <joyjehad28@gmail.com>
2024-12-07 21:24:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ giftWrap.addEventListener("click", unwrapGift);
 giftWrap.addEventListener("touchstart", unwrapGift);
 
 function unwrapGift() {
-    // Remove the gift wrap
-    giftWrap.style.display = "none";
+    // Hide the gift wrap
+    giftWrap.style.opacity = "0";
+    setTimeout(() => {
+        giftWrap.style.display = "none"; // Ensure it's fully hidden after fade-out
+    }, 500);
 
     // Show the letter
     letterContainer.classList.remove("hidden");
